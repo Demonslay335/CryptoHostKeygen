0ccb5c893bfd43eb24c1f2cb6d2c51940dceee7b
Michael T. Gillespie <mtgillespie@facettech.com>
2016-04-08 11:32:51 -0500

    Initial commit. Basic keygen for CryptoHost Ransomware.

---
+++ CryptoHostKeygen/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CryptoHostKeygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TwitterLink = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // PasswordTextbox
+            // 
+            this.PasswordTextbox.Location = new System.Drawing.Point(12, 25);
+            this.PasswordTextbox.Multiline = true;
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.Size = new System.Drawing.Size(260, 36);
+            this.PasswordTextbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your RAR Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(163, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Coded by";
+            // 
+            // TwitterLink
+            // 
+            this.TwitterLink.AutoSize = true;
+            this.TwitterLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwitterLink.Location = new System.Drawing.Point(205, 64);
+            this.TwitterLink.Name = "TwitterLink";
+            this.TwitterLink.Size = new System.Drawing.Size(67, 12);
+            this.TwitterLink.TabIndex = 3;
+            this.TwitterLink.TabStop = true;
+            this.TwitterLink.Text = "Demonslay335";
+            this.TwitterLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TwitterLink_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 85);
+            this.Controls.Add(this.TwitterLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CryptoHost Keygen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordTextbox;
+        private System.Windows.Forms.LinkLabel TwitterLink;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CryptoHostKeygen/Form1.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoHostKeygen
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            TwitterLink.Links.Add(0, TwitterLink.Text.Length, "https://twitter.com/demonslay335");
+            PasswordTextbox.Text = GenerateKey();
+        }
+
+        private void TwitterLink_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        public static void Decrypt()
+        {
+            String key = GenerateKey();
+            String rarPath = GetRarPath(key);
+
+            Interaction.Shell(string.Concat(new string[]
+            {
+                "processor X -o+ -p",
+               key,
+                " ",
+                rarPath,
+                Interaction.Environ("userprofile"),
+                "\\Desktop"
+            }), AppWinStyle.Hide, false, -1);
+        }
+
+        public static String GetRarPath(String key)
+        {
+            String appData = Interaction.Environ("appdata");
+            if(File.Exists(appData + "\\" + key))
+            {
+                return appData + "\\" + key;
+            }
+            else if(File.Exists(appData + "\\" + key + ".rar"))
+            {
+                return appData + "\\" + key + ".rar";
+            }
+            throw new FileNotFoundException("Encrypted data not found");
+        }
+
+        public static String GenerateKey()
+        {
+            String cpu = smethod_6();
+            String hdd = smethod_7("C");
+            String mobo = smethod_8();
+            String SHA1 = Conversions.ToString(smethod_9(cpu + hdd + mobo));
+            String username = Interaction.Environ("username");
+            return SHA1 + username;
+        }
+
+        public static string smethod_6()
+        {
+            string result = "";
+            try
+            {
+                string text = string.Empty;
+                SelectQuery query = new SelectQuery("Win32_processor");
+                ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(query);
+                try
+                {
+                    ManagementObjectCollection.ManagementObjectEnumerator enumerator = managementObjectSearcher.Get().GetEnumerator();
+                    while (enumerator.MoveNext())
+                    {
+                        ManagementObject managementObject = (ManagementObject)enumerator.Current;
+                        text = managementObject["processorId"].ToString();
+                    }
+                }
+                finally
+                {
+                    ManagementObjectCollection.ManagementObjectEnumerator enumerator = null;
+                    if (enumerator != null)
+                    {
+                        ((IDisposable)enumerator).Dispose();
+                    }
+                }
+                result = text;
+            }
+            catch (Exception arg_63_0)
+            {
+                ProjectData.SetProjectError(arg_63_0);
+                ProjectData.ClearProjectError();
+            }
+            return result;
+        }
+
+        internal static string smethod_7(string string_6 = "C")
+        {
+            string result = "";
+            try
+            {
+                ManagementObject managementObject = new ManagementObject(string.Format("win32_logicaldisk.deviceid=\"{0}:\"", string_6));
+                managementObject.Get();
+                result = managementObject["VolumeSerialNumber"].ToString();
+            }
+            catch (Exception arg_2A_0)
+            {
+                ProjectData.SetProjectError(arg_2A_0);
+                ProjectData.ClearProjectError();
+            }
+            return result;
+        }
+        internal static string smethod_8()
+        {
+            string result = "";
+            try
+            {
+                string text = string.Empty;
+                SelectQuery query = new SelectQuery("Win32_BaseBoard");
+                ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(query);
+                try
+                {
+                    ManagementObjectCollection.ManagementObjectEnumerator enumerator = managementObjectSearcher.Get().GetEnumerator();
+                    while (enumerator.MoveNext())
+                    {
+                        ManagementObject managementObject = (ManagementObject)enumerator.Current;
+                        text = managementObject["SerialNumber"].ToString();
+                    }
+                }
+                finally
+                {
+                    ManagementObjectCollection.ManagementObjectEnumerator enumerator = null;
+                    if (enumerator != null)
+                    {
+                        ((IDisposable)enumerator).Dispose();
+                    }
+                }
+                result = text;
+            }
+            catch (Exception arg_63_0)
+            {
+                ProjectData.SetProjectError(arg_63_0);
+                ProjectData.ClearProjectError();
+            }
+            return result;
+        }
+
+        public static object smethod_9(string string_6)
+        {
+            object result ="";
+            try
+            {
+                UTF8Encoding uTF8Encoding = new UTF8Encoding();
+                SHA1CryptoServiceProvider sHA1CryptoServiceProvider = new SHA1CryptoServiceProvider();
+                byte[] value = sHA1CryptoServiceProvider.ComputeHash(uTF8Encoding.GetBytes(string_6));
+                string_6 = BitConverter.ToString(value).Replace("-", "");
+                result = string_6;
+            }
+            catch (Exception arg_37_0)
+            {
+                ProjectData.SetProjectError(arg_37_0);
+                ProjectData.ClearProjectError();
+            }
+            return result;
+        }
+
+    }
+}
